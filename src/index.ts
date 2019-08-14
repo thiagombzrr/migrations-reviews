c2ea5432f87c2ed87c33d0f55fa64dd87d2940ed
Thiago Melo Bezerra <thiago.bezerra@zoop.com.br>
2019-08-14 09:54:32 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { Application } from 'probot'
+
+export = (app: Application) => {
+  // Your code here
+  app.log('Yay, the app was loaded!')
+
+  app.on('issues.opened', async context => {
+    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
+    context.github.issues.createComment(issueComment)
+  })
+
+  // For more information on building apps:
+  // https://probot.github.io/docs/
+
+  // To get your app running against GitHub, see:
+  // https://probot.github.io/docs/development/
+}
