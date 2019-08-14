c6859b4d453f96722cf12ce07737d1e1b097701b
Thiago Melo Bezerra <thiago.bezerra@zoop.com.br>
2019-08-14 12:21:02 -0300

    Config de manifest

--- src/pull-request-change.ts
+++ src/pull-request-change.ts
@@ -1,7 +1,7 @@
 import {Context} from "probot";
 
 export async function handlePullRequestChange (context: Context) {
-    context.log("Cheguei aqui")
+    context.log("Cheguei na funcao")
     let config: any = await context.config('migrations-reviews.yml')
 
     if (!config) {
@@ -9,18 +9,20 @@ export async function handlePullRequestChange (context: Context) {
         return
     }
 
+    context.log("Pegando PRs")
     const pullRequest = await getPullRequest(context)
+    context.log("Lendo PRs")
     const approvals = await getReviewsWithState(context, 'approved')
     const pendingReviewsCount = Math.max(0, config.reviewsUntilReady - approvals)
     const changesCount = pullRequest.additions + pullRequest.deletions
     const isReadyToMerge = (changesCount < config.changesThreshold) || (pendingReviewsCount === 0)
     const state = isReadyToMerge ? 'success' : config.notReadyState
-    const description = isReadyToMerge ? config.readyMessage : config.notReadyMessage
-
+    /*const description = isReadyToMerge ? config.readyMessage : config.notReadyMessage*/
+    context.log("Enviando status: " + state)
     return context.github.repos.createStatus(context.repo({
         sha: pullRequest.head.sha,
-        state: state,
-        description: description,
+        state: "pending",
+        description: "Pending review approvals",
         context: 'probot/migrations-reviews'
     }))
 }
@@ -29,7 +31,7 @@ async function getPullRequest(context: Context) {
     const response = await context.github.pulls.get({
         owner: context.payload.repository.owner.login,
         repo: context.payload.repository.name,
-        number: context.payload.pull_request.number
+        pull_number: context.payload.pull_request.number
     })
 
     return response.data
